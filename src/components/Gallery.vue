f430665580f3dea1350de50b06762ec5a3c59764
Tayna-Carvalho <144626954+Tayna-Carvalho@users.noreply.github.com>
2023-10-11 07:30:51 -0300

    put movie titlte at the gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,17 +1,22 @@
 <template>
 
     <section class="gallery">
-
+        
         <router-link 
             to="/detalhes"
             class="item" 
             v-for="(item,index) in this.itens" :key="index"
             @click="setStoreElements(item)" >
+                
+            <h3 class="title" v-if="item.media_type === 'tv'">{{ item.name }}</h3>
+            <h3 class="title" v-else>{{ item.title }}</h3>
+
+            <div class="dropShadow"></div>
 
             <img :src="'https://image.tmdb.org/t/p/w500/' + item.backdrop_path" :alt="item.title">
 
         </router-link>
-
+        
     </section>
 
     <router-view/>
@@ -33,6 +38,10 @@
                 this.$store.dispatch('loadCurrentVideo', item);
                 this.$store.dispatch('loadCurrentDetails', item);
                 this.$store.dispatch('loadCurrentCredits', item);
+            },
+
+            setFavorites (item) {
+                this.$store.dispatch('setFavorites', item);
             }
         }
     }
@@ -47,6 +56,18 @@
         gap: 32px;
     }
 
+    .gallery .item {
+        width: 414px;
+        height: 241.88px;
+
+        border-radius: 8px;
+        box-shadow: 0px 8px 10px 0px rgba(0, 0, 0, 0.15);
+
+        display: flex;
+        justify-content: left;
+        align-items: flex-end;
+    }
+
     .gallery .item img {
         width: 414px;
         height: 241.88px;
@@ -56,8 +77,37 @@
         flex-shrink: 0;
     }
 
-    .gallery .item img:hover {
+    .gallery .item .title {
+        color: white;
+        max-width: 414px;
+        padding:   0 16px 0 16px;
+        position: absolute;
+        z-index: 2;
+    }
+
+    .gallery .item .icon {
+        width: 32px;
+        height: 32px;
+        flex-shrink: 0;
+        position: absolute;
+        z-index: 3;
+    }
+
+    .gallery .item .dropShadow {
+        background: linear-gradient(180deg, rgba(27, 27, 27, 0.00) 0%, #1B1B1B 80.7%);
+        width: 100%;
+        height: 149px;
+        flex-shrink: 0;
+        position: absolute;
+        z-index: 1;
+    }
+
+    .gallery .item:hover {
         border: 3px solid var(--Roxo-Claro, #AC4DFF);
+    }
+
+    .gallery .item:hover .title {
+        color: var(--Roxo-Claro, #AC4DFF);
     }
 
 </style>
